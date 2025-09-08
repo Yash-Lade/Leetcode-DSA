77e0790d4ec3a4149c27e83063806a39b147e165
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2025-09-08 23:55:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (59.59%) - LeetSync

---
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    // helper funtion to check whether the no. contains zero
+     bool hasZero(int num) {
+        while (num > 0) {
+            if (num % 10 == 0) return true;
+            num /= 10;
+        }
+        return false;
+    }
+
+    vector<int> getNoZeroIntegers(int n) {
+        for (int a = 1; a < n; a++) {
+            int b = n - a;
+            if (!hasZero(a) && !hasZero(b)) {
+                return {a, b};
+            }
+        }
+        return {}; 
+    }
+};
