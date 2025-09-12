98f830e0e1917125167a3950d3987eee86c2ba11
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2025-09-12 22:29:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (52.54%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,47 +1,16 @@
 class Solution {
 public:
-
-    int lb(vector<int>& nums,int n, int target){
-        int low=0, high=n-1;
-        int first=n;
-        while(low<=high){
-            int mid= low + (high-low)/2;
-
-            
-            if(nums[mid]>=target){
-                first=mid;
-                high=mid-1;
-            }
-            else low=mid+1;
-        }
-        return first;
-    }   
-    int ub(vector<int>& nums,int n, int target){
-        int low=0, high=n-1;
-        int last=n;
-        while(low<=high){
-            int mid= low + (high-low)/2;
-
-            if(nums[mid]>target){
-                last=mid;
-                high=mid-1;
-            }
-            else low=mid+1;
-        }
-        return last;
-    }
-
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
-        int n=nums.size();
-    
-        int lbb = lb(nums, n, target);
-        int ubb = ub(nums, n, target)-1;
-        
-        if( lbb==n || nums[lbb]!=target) 
-        return {-1,-1};
-        
-        return {lbb,ubb};
+        int first = -1, last = -1;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==target && first == -1)
+            {
+                first = i;
+                last = i;
+            }
+            else if(nums[i] == target) 
+            last = i;
+        }
+        return {first, last};
     }
-
 };
