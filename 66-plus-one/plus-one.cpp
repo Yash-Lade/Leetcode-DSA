d94d698f951a712af5181c2a0d556a246934312c
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2025-09-17 17:22:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (69.89%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -2,16 +2,17 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int n = digits.size();
-        // Traverse from last digit
-        for (int i = n - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++; 
-                return digits; // done, no carry
+
+        for(int i = n-1 ; i>=0 ; i--){
+
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
             }
-            digits[i] = 0; // set current digit to 0 and continue carry
+            else 
+            digits[i] = 0;
         }
-        
-        // If all digits were 9 (like 999 -> 1000)
+        // if all digits are 999 etc..
         digits.insert(digits.begin(), 1);
         return digits;
     }
