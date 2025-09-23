2060975503fd017cfbce88ba6ebfe99af67c1f81
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2025-09-23 22:33:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (73.75%) - LeetSync

---
+++ 2556-convert-the-temperature/convert-the-temperature.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        
+        double kel = celsius + 273.15;
+        double fh = celsius * 1.80 + 32.00;
+
+
+        return {kel, fh};
+    }
+};
