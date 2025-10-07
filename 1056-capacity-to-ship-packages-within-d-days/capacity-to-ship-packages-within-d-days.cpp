06a263dfef9065a7055208a5627b2ea119d7e195
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2025-10-07 17:14:12 +0530

    Time: 948 ms (5.82%) | Memory: 35.1 MB (18.99%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int totalWeight(vector<int>& weights){
+        int allWeight = 0;
+        for(int i = 0; i< weights.size(); i++){
+            allWeight += weights[i];
+        }
+        return allWeight;
+    }
+    int minCap(vector<int>& weights){ // it is the max weight in the weights array
+        int maxi = INT_MIN;
+        for(int i = 0; i<weights.size(); i++){
+            if(maxi < weights[i]) maxi = weights[i];
+        }
+        return maxi;
+    }
+    int daysRequired(vector<int>& weights, int days, int cap){
+
+        int currDays = 1;
+        int load = 0;
+        for(int i = 0; i<weights.size(); i++){
+            
+            if(load + weights[i] > cap)
+            {
+                currDays++;
+                load = weights[i];
+            }
+            else{
+                load += weights[i];
+            }
+        }
+        return currDays;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int sumWeight = totalWeight(weights);
+        
+        if(days == 1) return sumWeight;
+        if(days == 2) return sumWeight/2;
+
+        
+        for(int cap = minCap(weights) ; cap <= sumWeight; cap++){
+            
+            int daysNeeded = daysRequired(weights, days, cap);
+            if(daysNeeded <= days)
+            return cap;
+        }
+        return -1;
+    }
+};
