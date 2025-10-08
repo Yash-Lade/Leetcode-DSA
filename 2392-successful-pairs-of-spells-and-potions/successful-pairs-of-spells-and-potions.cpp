3f5b1b4e3b38910158598cbfda0cbedd1b0cf2f8
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2025-10-08 16:29:13 +0530

    Time: 44 ms (53.89%) | Memory: 128.4 MB (8.69%) - LeetSync

---
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        
+        int m = spells.size();
+        int n = potions.size();
+        vector<int> ans;
+        sort(potions.begin(), potions.end());
+
+        for(int i = 0; i<m; i++){
+
+            int spell = spells[i];
+            // minPotion * spell >= success thereforce minPotion = success/spell
+            long long minPotion = (success + spell - 1)/ spell; // ceil of success/spell
+
+            // now if last element of postions is < minPotion then ans be 0
+            int maxPotion = potions[n-1];
+            if(maxPotion < minPotion){
+                ans.push_back(0);
+                continue; // skip the lower bound
+            } 
+
+            // this lower bound return pointer by default so do a substraction to get index
+            int index = lower_bound(potions.begin(), potions.end(), minPotion) - potions.begin(); // minPotion ka  Lowerbound nikalne ke liye
+
+            int count = n - index;
+            ans.push_back(count);
+        }
+        return ans;
+
+    }
+};
