f502a0e67684a642791bc88f7506607f2d49499c
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2025-12-01 23:06:48 +0530

    Time: 150 ms (40.00%) | Memory: 173.7 MB (40.00%) - LeetSync

---
+++ 4141-count-elements-with-at-least-k-greater-values/count-elements-with-at-least-k-greater-values.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countElements(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (k == 0) return n;            // every element has >= 0 greater elements
+
+        sort(nums.begin(), nums.end());
+        int qualified = 0;
+
+        for (int i = 0; i < n; ++i) {
+            // first index strictly greater than nums[i]
+            int idx = upper_bound(nums.begin(), nums.end(), nums[i]) - nums.begin();
+            int totalGreater = n - idx;
+            if (totalGreater >= k) 
+                qualified++;
+        }
+        return qualified;    
+    }
+};
