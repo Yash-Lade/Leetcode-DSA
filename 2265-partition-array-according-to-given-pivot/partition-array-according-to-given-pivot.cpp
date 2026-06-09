0cf930529b58be2489c2b2b6a2f032b774fc8d88
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2026-06-09 11:54:05 +0530

    Time: 7 ms (62.13%) | Memory: 133.4 MB (52.65%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -3,15 +3,21 @@ public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         
         vector<int> ans;
+        int pivotCount = 0;
 
-        for(auto num: nums)
-        if(num < pivot)
-        ans.push_back(num);
+        for(auto num: nums){
 
-        for(auto num: nums)
-        if(num == pivot)
-        ans.push_back(num);
+            if(num == pivot) pivotCount++;
 
+            if(num < pivot)
+            ans.push_back(num);
+        }
+        
+        while(pivotCount){
+            ans.push_back(pivot);
+            pivotCount--;
+        }
+        
         for(auto num: nums)
         if(num > pivot)
         ans.push_back(num);
