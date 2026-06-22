22978907dad80f9aa5a5c45596924aab5cb12076
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2026-06-22 12:00:46 +0530

    Time: 4 ms (2.59%) | Memory: 9.1 MB (39.61%) - LeetSync

---
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        
+        unordered_map<char, int> mpp;
+
+        for(auto c : text){
+            if(c == 'b' || c == 'a' || c == 'l' || c == 'o' || c == 'n')
+            mpp[c]++;
+        }
+        int instances = 0;
+        
+        int maxBalloons = min({
+            mpp['b'],
+            mpp['a'],
+            mpp['l'] / 2,
+            mpp['o'] / 2,
+            mpp['n']
+        });
+
+        return maxBalloons;
+    }
+};
