854955e3cc94595baf0a4bb9f86f371d1a753309
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2026-06-23 11:14:10 +0530

    Time: 2 ms (15.34%) | Memory: 8.9 MB (12.17%) - LeetSync

---
+++ 2372-rearrange-characters-to-make-target-string/rearrange-characters-to-make-target-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rearrangeCharacters(string s, string target) {
+        
+        unordered_map<char, int> mp1, mp2;
+        for (char c : s)      mp1[c]++;
+        for (char c : target) mp2[c]++;
+
+        int res = INT_MAX;
+        for (auto& [c, freq] : mp2) // here c = mp2.first and freq = mp2.second
+            res = min(res, mp1[c] / freq);
+
+        return res;
+    }
+};
